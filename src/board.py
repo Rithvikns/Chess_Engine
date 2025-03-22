e6e3a3816fdd166c80f1bcd9e4255021918f9ac6
Rithvikns <rithviknswamy@gmail.com>
2025-03-22 12:45:11 +0100

    changes

--- src/board.py
+++ src/board.py
@@ -2,29 +2,35 @@ from dataclasses import dataclass , field
 
 @dataclass
 class board:
-    board : list[int] = field(default_factory = list)
-
-    @staticmethod
-    def convert_notation_to_position(str):
-        if str[0] not in "abcdefgh":
-            return "wrong notation"
-        else:
-            if str[0] == 'a':
-                return (1,int(str[1]))
-            elif str[0] == 'b':
-                return (2,int(str[1]))
-            elif str[0] == 'c':
-                return (3,int(str[1]))
-            elif str[0] == 'd':
-                return (4,int(str[1]))
-            elif str[0] == 'e':
-                return (5,int(str[1]))
-            elif str[0] == 'f':
-                return (6,int(str[1]))
-            elif str[0] == 'g':
-                return (7,int(str[1]))
-            elif str[0] == 'h':
-                return (8,int(str[1]))
-            
-    def display_function():
-        print("chess Board is")
+    def display(self):
+        """Converts bitboards into a readable board format and prints it."""
+        pass
+    
+    def set_piece(self, square: int, piece: str):
+        """Updates the bitboard to add/remove a piece at a given square."""
+        pass
+    
+    def move_piece(self, start: int, end: int):
+        """Moves a piece from one square to another using bit manipulation."""
+        pass
+    
+    def is_square_occupied(self, square: int) -> bool:
+        """Checks if a square has any piece."""
+        pass
+    
+    def get_occupancy(self, color: str):
+        """Returns a combined bitboard of all pieces for a given color."""
+        pass
+    
+    def to_fen(self) -> str:
+        """Converts the bitboard position into FEN notation."""
+        pass
+    
+    @classmethod
+    def from_fen(cls, fen: str):
+        """Loads a position from a FEN string into bitboards."""
+        pass
+    
+    def position_hash(self) -> int:
+        """Generates a unique hash for the current board state."""
+        pass
