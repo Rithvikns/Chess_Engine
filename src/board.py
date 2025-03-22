73168ef03d156c5a0dea647f4707f5e6595e0ccf
Rithvikns <rithviknswamy@gmail.com>
2025-03-22 15:04:52 +0100

    added set piece function in board.py

--- src/board.py
+++ src/board.py
@@ -1,49 +1,39 @@
 from dataclasses import dataclass , field
-bitboards = {
-    'white_pawns'   : 0b00000000_00000000_00000000_00000000_00000000_00000000_11111111_00000000,
-    'white_knights' : 0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_01000010,
-    'white_rooks'   : 0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_10000001,
-    'white_bishops' : 0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00100100,
-    'white_queen'   : 0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00010000,
-    'white_king'    : 0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00001000,
-    'black_pawns'   : 0b00000000_11111111_00000000_00000000_00000000_00000000_00000000_00000000,
-    'black_knights' : 0b01000010_00000000_00000000_00000000_00000000_00000000_00000000_00000000,
-    'black_rooks'   : 0b10000001_00000000_00000000_00000000_00000000_00000000_00000000_00000000,
-    'black_bishops' : 0b00100100_00000000_00000000_00000000_00000000_00000000_00000000_00000000,
-    'black_queen'   : 0b00010000_00000000_00000000_00000000_00000000_00000000_00000000_00000000,
-    'black_king'    : 0b00001000_00000000_00000000_00000000_00000000_00000000_00000000_00000000
-  
-}
-bitboard_names = {
-        'white_pawns'   : 'P',
-        'white_knights' : 'N',
-        'white_rooks'   : 'R',
-        'white_bishops' : "B",
-        'white_queen'   : 'Q',
-        'white_king'    : 'K',
-        'black_pawns'   : 'p',
-        'black_knights' : 'n',
-        'black_rooks'   : 'r',
-        'black_bishops' : 'b',
-        'black_queen'   : 'q',
-        'black_king'    : 'k'
-}
+
 @dataclass
 class board:
-    def display(self,bitboards,bitboard_names):
+    bitboards : dict = field(default_factory=lambda: {
+    'P' : 0b00000000_00000000_00000000_00000000_00000000_00000000_11111111_00000000,
+    'N' : 0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_01000010,
+    'R' : 0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_10000001,
+    'B' : 0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00100100,
+    'Q' : 0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00010000,
+    'K' : 0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00001000,
+    'p' : 0b00000000_11111111_00000000_00000000_00000000_00000000_00000000_00000000,
+    'n' : 0b01000010_00000000_00000000_00000000_00000000_00000000_00000000_00000000,
+    'r' : 0b10000001_00000000_00000000_00000000_00000000_00000000_00000000_00000000,
+    'b' : 0b00100100_00000000_00000000_00000000_00000000_00000000_00000000_00000000,
+    'q' : 0b00010000_00000000_00000000_00000000_00000000_00000000_00000000_00000000,
+    'k' : 0b00001000_00000000_00000000_00000000_00000000_00000000_00000000_00000000
+  
+    })
+
+    def display(self):
         """Converts bitboards into a readable board format and prints it."""
         chess_board = [[0]*8 for _ in range(8)]
-        for board_name , bitboard in bitboards.items():
+        for board_name , bitboard in self.bitboards.items():
             for i in range(64):
-                if bitboard[i]:
-                    row = i//8
-                    col = i%8
-                    chess_board[row][col] = bitboard_names[board_name]
-        print(chess_board)
+                if ((bitboard >> i) & 1):
+                    row = 7 - (i//8)
+                    col = i % 8
+                    chess_board[row][col] = board_name
+        for row in chess_board:
+            print(row)
         pass
     
     def set_piece(self, square: int, piece: str):
         """Updates the bitboard to add/remove a piece at a given square."""
+        self.bitboards[piece] = self.bitboards[piece] | (1 << square)
         pass
     
     def move_piece(self, start: int, end: int):
