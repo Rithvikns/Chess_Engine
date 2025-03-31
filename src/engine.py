199f708ddaebc0871ba591850bc0eab3fd5750ad
Rithvikns <rithviknswamy@gmail.com>
2025-03-31 18:04:47 +0200

    modifying engine module

--- src/engine.py
+++ src/engine.py
@@ -9,27 +9,77 @@ class Engine:
 
     def generate_legal_moves(self):
         """Generates all legal moves for the current position."""
+        legal_moves = []
+        
         if board.Board.side_to_move == 'w':
             self.color = 'white'
         else:
             self.color = 'black'
+        
         own_pieces = board.Board.get_occupancy(self.color)
         enemy_pieces = board.Board.get_occupancy('black' if self.color == 'white' else 'white')
         all_pieces = own_pieces | enemy_pieces
+        
         if board.Board.enpassant != '-':
             ep_square = board.Board.get_enpassant_square()
+        else:
+            ep_square = None
         
-        # pawn moves
-
-        pawns = board.Board.bitboards('P') if self.color == 'white' else 'black'
+        # Pawn moves
+        pawns = board.Board.bitboards('P' if self.color == 'white' else 'P')
         push_dir = 8 if self.color == 'white' else -8
-        promotion_rank = (pawns & 0xFF00000000000000) if self.color == 'white' else (pawns & 0x00000000000000FF)
-
+        promotion_rank = 0xFF00000000000000 if self.color == 'white' else 0x00000000000000FF
+        
         for pawn in pawns:
+            # Single push
             target = pawn + push_dir
             if target not in all_pieces:
                 if target & promotion_rank:
-                    pass
+                    if self.color == 'white':
+                        legal_moves.append((pawn, target, 'Q'))  # White Queen
+                        legal_moves.append((pawn, target, 'R'))  # White Rook
+                        legal_moves.append((pawn, target, 'B'))  # White Bishop
+                        legal_moves.append((pawn, target, 'N'))  # White Knight
+                    else:
+                        legal_moves.append((pawn, target, 'q'))  # Black Queen
+                        legal_moves.append((pawn, target, 'r'))  # Black Rook
+                        legal_moves.append((pawn, target, 'b'))  # Black Bishop
+                        legal_moves.append((pawn, target, 'n'))  # Black Knight
+                else:
+                    legal_moves.append((pawn, target))
+                
+                # Double push from starting rank
+                if (self.color == 'white' and (pawn & 0x000000000000FF00)) or (self.color == 'black' and (pawn & 0x00FF000000000000)):
+                    double_push = target + push_dir
+                    if double_push not in all_pieces:
+                        legal_moves.append((pawn, double_push))
+            
+            # Captures
+            for attack_dir in [7, 9] if self.color == 'white' else [-7, -9]:
+                target = pawn + attack_dir
+                if target in enemy_pieces:
+                    if target & promotion_rank:
+                        if self.color == 'white':
+                            legal_moves.append((pawn, target, 'Q'))
+                            legal_moves.append((pawn, target, 'R'))
+                            legal_moves.append((pawn, target, 'B'))
+                            legal_moves.append((pawn, target, 'N'))
+                        else:
+                            legal_moves.append((pawn, target, 'q'))
+                            legal_moves.append((pawn, target, 'r'))
+                            legal_moves.append((pawn, target, 'b'))
+                            legal_moves.append((pawn, target, 'n'))
+                    else:
+                        legal_moves.append((pawn, target))
+                
+                # En passant
+                if ep_square and target == ep_square:
+                    legal_moves.append((pawn, target, 'ep'))
+        
+        # TODO: Add moves for knights, bishops, rooks, queens, and kings.
+        
+        return legal_moves
+
 
     def is_move_legal(self, board, move: tuple) -> bool:
         """Checks if a move is legal."""
